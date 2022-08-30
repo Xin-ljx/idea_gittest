dd8e1226561eb9ef071210e37393875c9fc9564a
Xin <878583713@qq.com>
2022-08-30 20:40:58 +0800

    第二次添加

--- src/main/java/com/ljx/git/GitTest.java
+++ src/main/java/com/ljx/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ljx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("这是添加的第一次内容");
+        System.out.println("这是添加的第二次内容");
     }
 }
